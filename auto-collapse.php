76a94676da0bdee1cab1ddd47af18753981a017e
bungrahman <44845710+bungrahman@users.noreply.github.com>
2023-04-19 10:45:05 +0700

    Update auto-collapse.php

--- auto-collapse.php
+++ auto-collapse.php
@@ -1,1 +1,30 @@
 
+/* ----- Custom Admin Folded ----- */
+function custom_admin_folded() { ?>
+  <script>
+  jQuery.noConflict();
+  (function( $ ) {
+  
+  // Start code using $ as alias to jQuery   
+  $(document).ready(function() {
+    $('body').addClass('folded');
+  });
+  // End code using $ as alias to jQuery
+  
+  // Start code using $ as alias to jQuery   
+  $(function() {
+    $('#adminmenuwrap').hover(function() {
+    $('body').removeClass('folded');
+    });
+    $('#wpcontent').hover(function() {
+    $('body').addClass('folded');    
+    });
+  });
+  // End code using $ as alias to jQuery
+  
+  })(jQuery);
+  </script>  
+  <?php
+  }
+  
+  add_action( 'admin_head','custom_admin_folded' );
